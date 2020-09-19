c6f78d907f25c8934abea35dc1e0fb31fb93d624
Rifki Rizkia <rifkirizkia91@gmail.com>
2020-09-19 14:47:26 +0700

    PermenDira.java

---
+++ PermenDira.java
@@ -0,0 +1,17 @@
+public class PermenDira {
+	public static void main(String[] args) {
+		//jumlah permen Dira dan jumlah teman Dira
+		int PERMEN_DIRA = 25;
+		int TEMAN_DIRA = 4;
+		int TOTAL_ORANG = 5;
+		
+		//hasil pembagian permen Dira
+		int TOTAL_PERMEN = PERMEN_DIRA / TOTAL_ORANG;
+		
+		System.out.println("Dira akan membagikan permen kepada temannya");
+		System.out.println("Permen Dira = 25");
+		System.out.println("Teman Dira = 4");
+		System.out.println("Total permen yang dibagikan =" + TOTAL_PERMEN);
+	}
+}
+		
